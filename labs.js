3a82080ca18f8dfb4e3e3b68043fd865ea9dc902
Alistair MacDonald <amacdonald@aisquared.com>
2014-09-28 08:36:19 -0500

    Adding in scripture JSONP service that pulls from the NET bible.

--- labs.js
+++ labs.js
@@ -1,25 +1,23 @@
-(function(){
+(function (){
 
-  // Mode ENUMS
-  var PAINT = 1
-    , ERASE = 2
-    ;
+  'use strict';
 
-  var settings = {
-      background: "#000"
-    , textColor: "#FF0"
-    , scriptureServicePath: "http://labs.bible.org/api/?passage="
-    , scriptureServiceOpts: "&type=json"
-    , defaultScripture: "John%203:16-17"
-  };
+  // Main Settings  
+  var scriptureServicePath = 'http://labs.bible.org/api/?passage=',
+    scriptureServiceOpts = '&type=json',
+    defaultScripture = 'Romans%208:28-30',
 
-  var docElem = document.documentElement
-    , width =  0
-    , height =  0
-    , context = paint.getContext('2d')
-    , highlightColor = "yellow"
-    , mode = PAINT
-    ;
+    // ENUMS
+    PAINT = 1, 
+    ERASE = 2, 
+  
+    // Other Variables
+    docElem = document.documentElement,
+    width =  0,
+    height =  0,
+    context = paint.getContext('2d'),
+    highlightColor = 'yellow',
+    mode = PAINT;
 
 
 
@@ -40,31 +38,29 @@
   }
 
   function formatScripture(e){
-    var scriptureMarkup = '<h1>'+unescape(settings.defaultScripture)+'</h1> ';
+    var scriptureMarkup = '<h1>'+unescape(defaultScripture)+'</h1> ';
 
     e.forEach(function(scripture){
       scriptureMarkup+='<p><sup>'+scripture.verse+'</sup> <span>'+scripture.text+'</span></p>'
     });
-    // console.log(scriptureMarkup);
     text.innerHTML = scriptureMarkup;
     setupCanvas();
   }
 
   function getScripture(){
-    var url = settings.scriptureServicePath + settings.defaultScripture + settings.scriptureServiceOpts;
+    var url = scriptureServicePath + defaultScripture + scriptureServiceOpts;
     
-    // $.ajax({
-    //   url: url,
-    //   dataType: 'jsonp',
-    //   success: function(e){
-    //     formatScripture(e);
-    //   }
-    // });
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(e){
+        formatScripture(e);
+      }
+    });
 
-    // text.innerHTML = '<h1>John 3:16-17</h1> <p><sup>16</sup> <span>For this is the way God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life.</span></p><p><sup>17</sup> <span>For God did not send his Son into the world to condemn the world, but that the world should be saved through him. <a style="" target="_blank" href="http://bible.org/page.php?page_id=3537">&copy;NET</a></span></p>';
-    text.innerHTML = '<h1>Romans 8:26-27</h1> <p><sup>26</sup> Likewise the Spirit helps us in our weakness. For we do not know what to pray for as we ought, but the Spirit himself intercedes for us with groanings too deep for words.</p> <p><sup>27</sup>And he who searches hearts knows what is the mind of the Spirit, because the Spirit intercedes for the saints according to the will of God. </p>';
-    setupCanvas();
-    // text.innerHTML = '<h1></h1><sup>16</sup> <span>For this is the way God loved the world: He gave his one and only Son, so that everyone who believes in him will not perish but have eternal life.</span> <sup>17</sup> <span>For God did not send his Son into the world to condemn the world, but that the world should be saved through him. <a target="_blank" href="http://bible.org/page.php?page_id=3537">&copy;NET</a></span>';
+
+    // text.innerHTML = '<h1>Romans 8:26-27</h1> <p><sup>26</sup> Likewise the Spirit helps us in our weakness. For we do not know what to pray for as we ought, but the Spirit himself intercedes for us with groanings too deep for words.</p> <p><sup>27</sup>And he who searches hearts knows what is the mind of the Spirit, because the Spirit intercedes for the saints according to the will of God. </p>';
+    // setupCanvas();
   }
   
 
